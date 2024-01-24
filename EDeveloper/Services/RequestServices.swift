8f1bf670858154527968fe458eacb4692de3a11f
Nagaraju0323 <nagarajukios85@gmail.com>
2024-01-24 23:53:04 +0530

    test_URLRequestload

--- EDeveloper/Services/RequestServices.swift
+++ EDeveloper/Services/RequestServices.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+class RequestServices {
+    let httpRequet : HttpRequest
+    init(httpRequet: HttpRequest) {
+        self.httpRequet = httpRequet
+    }
+    
+    func load() {
+        httpRequet.apirequest(requesturl: URL(string: "http://google.com")!)
+    }
+}
+
 
 class HttpRequest {
-
     var url : URL?
-    
+    func apirequest(requesturl:URL) {
+        url = requesturl
+    }
 }
 
 
--- EDeveloperTests/EDeveloperTests.swift
+++ EDeveloperTests/EDeveloperTests.swift
@@ -9,9 +9,16 @@ import XCTest
 @testable import EDeveloper
 final class EDeveloperTests: XCTestCase {
 
-    func test_ApiRequestload() throws {
+    func test_ApiRequestload_Empty() throws {
         let client  = HttpRequest()
         XCTAssertNil(client.url)
+    }
+    
+    func test_URLRequestload() throws {
+        let client  = HttpRequest()
+        let sut = RequestServices(httpRequet: client)
+        sut.load()
+        XCTAssertNotNil(client.url)
     }
     
 
